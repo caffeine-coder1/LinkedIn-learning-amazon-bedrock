d9b605c004daba0f55403c74033c226d92254035
satya karthik ramachandrapurapu <40475023+caffeine-coder1@users.noreply.github.com>
2024-05-19 13:31:16 +0000

    lang chain exercise

--- 02_05b/langchain-bedrock-sample.py
+++ 02_05b/langchain-bedrock-sample.py
@@ -1,11 +1,26 @@
 #Imports
+import json
+import boto3
+from langchain.llms.bedrock import Bedrock
 
 #Create the bedrock client
-
+client = boto3.client("bedrock-runtime")
 #setting model inference parameters
+model_id = "amazon.titan-text-express-v1"
+prompt = """as a hiring manager of a company, you need to write a welcoming mail to the new joinee. write the mail.
+"""
+
+model_kwargs = {
+        "temperature": 0.1,  
+        "topP": 1,
+        "maxTokenCount": 256
+    }
 
 #Create the llm
+my_llm = Bedrock(client=client, model_id=model_id, model_kwargs=model_kwargs)
 
 #Generate the response
+respose = my_llm.invoke(prompt)
 
 #Display the result
+print(f"generated text: \n{respose}")
