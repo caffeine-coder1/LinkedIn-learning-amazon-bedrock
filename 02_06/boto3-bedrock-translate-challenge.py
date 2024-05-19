6c81bb26aafcb7d0a63e905ca3cc6f7de50b5eb5
satya karthik ramachandrapurapu <40475023+caffeine-coder1@users.noreply.github.com>
2024-05-19 13:32:04 +0000

    translation challenge code

--- 02_06/boto3-bedrock-translate-challenge.py
+++ 02_06/boto3-bedrock-translate-challenge.py
@@ -5,17 +5,31 @@ import json
 #Create the client
 client = boto3.client(service_name='bedrock-runtime')
 
+# Define the user message to send.
+user_message = """Translate the sentence from English to French.
+    English: Learning about Generative Al is fun and exciting using Amazon Bedrock.
+    French: """
+
+# Embed the message in Llama 3's prompt format.
+prompt = f"""
+<|begin_of_text|>
+<|start_header_id|>user<|end_header_id|>
+{user_message}
+<|eot_id|>
+<|start_header_id|>assistant<|end_header_id|>
+"""
+
 #Construct the body
 #specify your prompt
 body = json.dumps({
-    "prompt": "", 
-    "maxTokens": 200,
+    "prompt": prompt, 
+    "max_gen_len": 512,
     "temperature": 0.5,
-    "topP": 0.5
+    "top_p": 0.9,
 })
 
 #Specify model id and content types
-modelId = ''
+modelId = 'meta.llama3-8b-instruct-v1:0'
 accept = 'application/json'
 contentType = 'application/json'
 
@@ -31,4 +45,4 @@ response = client.invoke_model(
 response_body = json.loads(response.get('body').read())
 
 #Display the output
-print(response_body.get('completions')[0].get('data').get('text'))
+print(response_body["generation"])
