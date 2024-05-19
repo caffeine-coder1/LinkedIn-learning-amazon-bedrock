ad4f33a74af8ca75cb7c3195720d315eeefbeb0e
satya karthik ramachandrapurapu <40475023+caffeine-coder1@users.noreply.github.com>
2024-05-19 08:25:34 +0000

    called bedrock from python file

--- 02_03b/boto3-bedrock-sample.py
+++ 02_03b/boto3-bedrock-sample.py
@@ -1,13 +1,33 @@
 #Imports
-
+import boto3
+import json
 #Create the bedrock client
+client = boto3.client("bedrock-runtime")
 
 #Setting the prompt
-
+prompt = """Command: you are a freshman in college. write your assignment on genrative Ai.
+Assignment:
+"""
 #Model specification
-
+model_id = "amazon.titan-text-lite-v1"
 #Configuring parameters to invoke the model
-
+body = json.dumps({
+  "inputText": prompt,
+  "textGenerationConfig":{
+    "maxTokenCount":256
+  }
+})
 #Invoke the model
+response = client.invoke_model(
+  body=body, modelId=model_id
+)
+
+print(f"respose: \n{response}\n")
+print(f"respose type: {type(response)}\n")
 
 #Parsing and displaying the output
+
+respose_body = json.loads(response.get("body").read())
+generated_text = respose_body.get("results")[0].get("outputText")
+
+print(f"generated text: \n{generated_text}")
